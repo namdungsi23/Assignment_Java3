283481a0c806873842d9ef8a4755a2d18edce496
PS46522_PhanHuuKhang <phanhuukhang4545@gmail.com>
2025-10-18 16:34:32 +0700

    Update JDBC.java

--- src/main/java/utils/JDBC.java
+++ src/main/java/utils/JDBC.java
@@ -1,8 +1,5 @@
 package utils;
 
-
-
-
 import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
@@ -15,49 +12,52 @@ import java.sql.SQLException;
  * Servlet implementation class JDBC
  */
 public class JDBC {
-	static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	static String dburl = "jdbc:sqlserver://localhost:1433;databaseName=FPolyNews;encrypt=false";
-	static String username = "sa";
-	static String password = "";
-	static {
-		try { // nạp driver
-			Class.forName(driver);
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	/** Mở kết nối */
-	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(dburl, username, password);
-	}
+    static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    static String dburl = "jdbc:sqlserver://localhost:1433;databaseName=FPolyNews;encrypt=false";
+    static String username = "sa";
+    static String password = "";
 
-	/** Thao tác dữ liệu */
-	public static int executeUpdate(String sql, Object... values) throws SQLException {
-		Connection connection = getConnection();
-		PreparedStatement statement = connection.prepareStatement(sql);
-		for (int i = 0; i < values.length; i++) {
-			statement.setObject(i + 1, values[i]);
-		}
-		return statement.executeUpdate();
-	}
+    static {
+        try { // nạp driver
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/** Truy vấn dữ liệu */
-	public static ResultSet executeQuery(String sql, Object... values) throws SQLException {
-		Connection connection = getConnection();
-		PreparedStatement statement = connection.prepareStatement(sql);
-		for (int i = 0; i < values.length; i++) {
-			statement.setObject(i + 1, values[i]);
-		}
-		return statement.executeQuery();
-	}
-	
-	/* goi  hàm */
-	public static ResultSet executeCallQuery(String sql, Object... values) throws SQLException {
-		Connection connection = getConnection();
-		CallableStatement statement = connection.prepareCall(sql);
-		for (int i = 0; i < values.length; i++) {
-			statement.setObject(i + 1, values[i]);
-		}
-		return statement.executeQuery();
-	}
+    /** Mở kết nối */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(dburl, username, password);
+    }
+
+    /** Thao tác dữ liệu */
+    public static int executeUpdate(String sql, Object... values) throws SQLException {
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            for (int i = 0; i < values.length; i++) {
+                statement.setObject(i + 1, values[i]);
+            }
+            return statement.executeUpdate();
+        }
+    }
+
+    /** Truy vấn dữ liệu */
+    public static ResultSet executeQuery(String sql, Object... values) throws SQLException {
+        Connection connection = getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        for (int i = 0; i < values.length; i++) {
+            statement.setObject(i + 1, values[i]);
+        }
+        return statement.executeQuery();
+    }
+
+    /* Goi hàm */
+    public static ResultSet executeCallQuery(String sql, Object... values) throws SQLException {
+        Connection connection = getConnection();
+        CallableStatement statement = connection.prepareCall(sql);
+        for (int i = 0; i < values.length; i++) {
+            statement.setObject(i + 1, values[i]);
+        }
+        return statement.executeQuery();
+    }
 }
