e1314d81b6dc1e7399cf2ce21080d28922527cf5
PS46522_PhanHuuKhang <phanhuukhang4545@gmail.com>
2025-10-18 16:35:20 +0700

    Update UserControl.java

--- src/main/java/views/UserControl.java
+++ src/main/java/views/UserControl.java
@@ -6,60 +6,121 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
 import implement.TrangchuDaoImpl;
 import dao.TrangchuDao;
 import entity.News;
+
 /**
  * Servlet implementation class UserControl
  */
 @WebServlet("/UserControl")
 public class UserControl extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private TrangchuDao dao = new TrangchuDaoImpl(); 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private static final long serialVersionUID = 1L;
+    private TrangchuDao dao = new TrangchuDaoImpl(); // Khởi tạo DAO
+
     public UserControl() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		 String page = request.getParameter("page");
-	        if (page == null) page = "home";
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String page = request.getParameter("page");
+        String id = request.getParameter("id");
+        String action = request.getParameter("action");
 
-	        String file;
-	        switch (page) {
-	        		case "home": file = "/home-control"; break;
-	            case "vanhoa":  file = "/van-hoa"; break;
-	            case "thethao": file = "/the-thao"; break;
-	            case "suckhoe": file = "/suc-khoe"; break;
-	            case "chinhtri":file = "/chinh-tri"; break;
-	            case "dulich":  file = "/du-lich"; break;
-	            case "doisong":  file = "/doi-song"; break;
-	            case "giaoduc":  file = "/giao-duc"; break;
-	            case "congnghe":  file = "/cong-nghe"; break;
-	            default:        file = "/home-control";
-	        }
+        if (page == null) page = "home";
 
-	        // Gửi tên file sang JSP chính
-	        request.setAttribute("contentPage", file);
-	        request.getRequestDispatcher("/manager/Reader.jsp").forward(request, response);
+        String file;
+        switch (page) {
+            case "home":
+                if (id != null && "detail".equals(action)) {
+                    News news = dao.findById(id);
+                    if (news != null) {
+                        dao.updateViewCount(id); 
+                        request.setAttribute("news", news);
+                    } else {
+                        System.out.println("Không tìm thấy tin tức với id = " + id);
+                    }
+                    file = "/manager/Home.jsp"; 
+                } else {
+                    file = "/home-control"; 
+                    System.out.println("Đang chuyển hướng tới /home-control");
+                }
+                break;
+            case "vanhoa":
+                List<News> newsListVanhoa = dao.findNewsByCategory("CAT04");
+                if (newsListVanhoa == null) {
+                    newsListVanhoa = new ArrayList<>(); 
+                }
+                request.setAttribute("newsList", newsListVanhoa);
+                file = "/manager/Vanhoa.jsp";
+                break;
+            case "thethao":
+                List<News> newsListThethao = dao.findNewsByCategory("CAT03");
+                if (newsListThethao == null) {
+                    newsListThethao = new ArrayList<>(); 
+                }
+                request.setAttribute("newsList", newsListThethao);
+                file = "/manager/Thethao.jsp";
+                break;
+            case "suckhoe":
+                List<News> newsListSuckhoe = dao.findNewsByCategory("CAT06");
+                if (newsListSuckhoe == null) {
+                    newsListSuckhoe = new ArrayList<>(); 
+                }
+                request.setAttribute("newsList", newsListSuckhoe);
+                file = "/manager/Suckhoe.jsp";
+                break;
+            case "chinhtri":
+                List<News> newsListChinhtri = dao.findNewsByCategory("CAT01");
+                if (newsListChinhtri == null) {
+                    newsListChinhtri = new ArrayList<>(); 
+                }
+                request.setAttribute("newsList", newsListChinhtri);
+                file = "/manager/Chinhtri.jsp";
+                break;
+            case "dulich":
+                List<News> newsListDulich = dao.findNewsByCategory("CAT04");
+                if (newsListDulich == null) {
+                    newsListDulich = new ArrayList<>(); 
+                }
+                request.setAttribute("newsList", newsListDulich);
+                file = "/manager/Dulich.jsp";
+                break;
+            case "doisong":
+                List<News> newsListDoisong = dao.findNewsByCategory("CAT05");
+                if (newsListDoisong == null) {
+                    newsListDoisong = new ArrayList<>(); 
+                }
+                request.setAttribute("newsList", newsListDoisong);
+                file = "/manager/Doisong.jsp";
+                break;
+            case "giaoduc":
+                List<News> newsListGiaoduc = dao.findNewsByCategory("CAT07");
+                if (newsListGiaoduc == null) {
+                    newsListGiaoduc = new ArrayList<>(); 
+                }
+                request.setAttribute("newsList", newsListGiaoduc);
+                file = "/manager/Giaoduc.jsp";
+                break;
+            case "congnghe":
+                List<News> newsListCongnghe = dao.findNewsByCategory("CAT08");
+                if (newsListCongnghe == null) {
+                    newsListCongnghe = new ArrayList<>(); 
+                }
+                request.setAttribute("newsList", newsListCongnghe);
+                file = "/manager/Congnghe.jsp";
+                break;
+            default:
+                file = "/manager/Home.jsp";
+        }
 
-	}
+        request.setAttribute("contentPage", file);
+        request.getRequestDispatcher("/manager/Reader.jsp").forward(request, response);
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
